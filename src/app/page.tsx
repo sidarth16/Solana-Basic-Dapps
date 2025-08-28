438ec4944e7000faaa2af82423efb5d5ea26e7a6
sidarth16 <ssidarth1999@gmail.com>
2025-08-29 01:45:13 +0530

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,8 @@ export default function HomePage() {
 
 
   return (
-    <div className="flex flex-col items-center gap-6">
+    <div className="flex flex-col items-center gap-6 min-h-[70vh] p-7">
+    {/* <div className="flex flex-col items-center min-h-screen p-4 bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100"></div> */}
       <h1 className="text-3xl font-bold">🚀 MiniSwap</h1>
       <TokenInputs tokenA={tokenA} setTokenA={setTokenA} tokenB={tokenB} setTokenB={setTokenB} />
       <PoolStatus poolStatus={poolStatus} />
